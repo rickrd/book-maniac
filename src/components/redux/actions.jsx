9c0b10cbfce42a2ffe09bf8a56f4a406d224a72c
rickrd <desouzaricardoc@gmail.com>
2019-12-23 00:06:12 -0300

    added createBook action

--- src/components/redux/actions.jsx
+++ src/components/redux/actions.jsx
@@ -1,5 +1,10 @@
 export const UPDATE_READ = 'UPDATE_READ'
+export const CREATE_BOOK = 'CREATE_BOOK'
 
 export const updateRead = index => {
   return { type: UPDATE_READ, index }
+}
+
+export const createBook = book => {
+  return { type: CREATE_BOOK, book }
 }
